7adb46e8cac5770ec36d94226809d3abb323afc4
Jonathan <jtellkampf@gmail.com>
2021-03-23 20:58:21 +0000

    Completed register resolver with validation and database persistence to enable user registration

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { UserInputError } = require("apollo-server");
 
 const resolvers = {
   Query: {
@@ -14,17 +15,37 @@ const resolvers = {
   Mutation: {
     register: async (parent, args, ctx, info) => {
       const { username, email, password, confirmPassword } = args;
+      let errors = {};
 
       try {
         // Validate input
+        if (email.trim() === "") errors.email = "Email must not be empty";
+        if (username.trim() === "")
+          errors.username = "Username must not be empty";
+        if (password.trim() === "")
+          errors.password = "Password must not be empty";
+        if (confirmPassword.trim() === "")
+          errors.confirmPassword = "Confirm password must not be empty";
 
+        if (password !== confirmPassword)
+          errors.confirmPassword = "Passwords must match";
+        // Check if username or email is unique
+        const userByEmail = await User.find({ email });
+        const userByUsername = await User.find({ username });
+
+        if (userByUsername.length > 0) errors.username = "Username is taken";
+        if (userByEmail.length > 0) errors.email = "Email is taken";
+
+        if (Object.keys(errors).length > 0) {
+          throw errors;
+        }
         // Create User
         const user = new User({ username, email, password });
         await user.save();
         return user;
       } catch (error) {
         console.log(error);
-        throw error;
+        throw new UserInputError("Bad input", { errors: error });
       }
     }
   }
