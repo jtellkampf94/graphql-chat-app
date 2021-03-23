c47c35eab001599edb0b342d27996db2848014fa
Jonathan <jtellkampf@gmail.com>
2021-03-23 11:02:55 +0000

    Created hashed Password, salt and profile picture url fields in user model as well as passpword hash and authentication functions in model

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 
 const UserSchema = new mongoose.Schema(
   {
@@ -12,10 +13,48 @@ const UserSchema = new mongoose.Schema(
       unique: "Email already exists",
       match: [/.+\@.+\..+/, "Please fill a valid email address"],
       required: "Email is required"
+    },
+    salt: String,
+    hashedPassword: {
+      type: String,
+      required: "Password is required"
+    },
+    profilePictureUrl: {
+      type: String
     }
   },
   { timestamps: true }
 );
+
+UserSchema.virtual("password")
+  .set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashedPassword = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+UserSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashedPassword;
+  },
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  }
+};
 
 const User = mongoose.model("user", UserSchema);
 
