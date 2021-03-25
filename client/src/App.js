7cb177b13b30ca80ea15a344b7cbcfb27784de89
Jonathan <jtellkampf@gmail.com>
2021-03-25 15:17:46 +0000

    Created Home and Login page components, implemented routing to route to these pages on app.js using react-router-dom package

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
 import Register from "./pages/Register";
-import "./App.scss";
+import Login from "./pages/Login";
 import ApolloProvider from "./ApolloProvider";
+import "./App.scss";
 
 function App() {
   return (
     <ApolloProvider>
-      <Container>
-        <Register />
-      </Container>
+      <BrowserRouter>
+        <Container className="pt-5">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Switch>
+        </Container>
+      </BrowserRouter>
     </ApolloProvider>
   );
 }
