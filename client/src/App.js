f2150315ea155fe66b0823fe5488abb2b05692ed
Jonathan <jtellkampf@gmail.com>
2021-03-27 17:16:19 +0000

    Created auth context and reducer using react hooks, made components in the app able to access context with its provider, made login component able to dispatch action to change the state in the auth context

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,23 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ApolloProvider from "./ApolloProvider";
+import { AuthProvider } from "./context/auth";
 import "./App.scss";
 
 function App() {
   return (
     <ApolloProvider>
-      <BrowserRouter>
-        <Container className="pt-5">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/register" component={Register} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </Container>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Container className="pt-5">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          </Container>
+        </BrowserRouter>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
