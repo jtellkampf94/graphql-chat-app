057fe532915382becb0aeb41d52cdd506bbe906f
Jonathan <jtellkampf@gmail.com>
2021-03-28 23:06:02 +0100

    Completed login functionality using the context hooks and useReducer hook, created a DynamicRoute component in the util directory which redirects you if not logged in or token expired, or if already loggen in or registered

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import ApolloProvider from "./ApolloProvider";
 import { AuthProvider } from "./context/auth";
+import DynamicRoute from "./util/DynamicRoute";
 import "./App.scss";
 
 function App() {
@@ -16,9 +17,9 @@ function App() {
         <BrowserRouter>
           <Container className="pt-5">
             <Switch>
-              <Route exact path="/" component={Home} />
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
+              <DynamicRoute exact path="/" component={Home} authenticated />
+              <DynamicRoute path="/register" component={Register} guest />
+              <DynamicRoute path="/login" component={Login} guest />
             </Switch>
           </Container>
         </BrowserRouter>
