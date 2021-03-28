57f7b1759fc30cf20e2c339a768062bcce9ee96c
Jonathan <jtellkampf@gmail.com>
2021-03-28 23:21:51 +0100

    Made the apollo-provider able to send the token in request header from the client, enabled the home component able to get users with the getUser graphql query using the token in the request header

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,20 @@
-import React from "react";
-import { Row, Button } from "react-bootstrap";
+import React, { Fragment } from "react";
+import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 
 import { useAuthDispatch } from "../context/auth";
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      id
+      username
+      email
+      createdAt
+    }
+  }
+`;
 
 const Home = ({ history }) => {
   const dispatch = useAuthDispatch();
@@ -12,18 +24,49 @@ const Home = ({ history }) => {
     history.push("/login");
   };
 
+  const { loading, data, error } = useQuery(GET_USERS);
+
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+
+  let usersMarkup;
+  if (!data || loading) {
+    usersMarkup = <p>Loading..</p>;
+  } else if (data.getUsers.length === 0) {
+    usersMarkup = <p>No users have joined yet</p>;
+  } else if (data.getUsers.length > 0) {
+    usersMarkup = data.getUsers.map(user => (
+      <div key={user.username}>
+        <p>{user.username}</p>
+      </div>
+    ));
+  }
+
   return (
-    <Row className="bg-white justify-content-around mb-1">
-      <Link to="/login">
-        <Button variant="link">Login</Button>
-      </Link>
-      <Link to="/register">
-        <Button variant="link">Register</Button>
-      </Link>
-      <Button variant="link" onClick={logout}>
-        Logout
-      </Button>
-    </Row>
+    <Fragment>
+      <Row className="bg-white justify-content-around mb-1">
+        <Link to="/login">
+          <Button variant="link">Login</Button>
+        </Link>
+        <Link to="/register">
+          <Button variant="link">Register</Button>
+        </Link>
+        <Button variant="link" onClick={logout}>
+          Logout
+        </Button>
+      </Row>
+      <Row className="bg-white">
+        <Col xs={4}>{usersMarkup}</Col>
+        <Col xs={8}>
+          <p>Messages</p>
+        </Col>
+      </Row>
+    </Fragment>
   );
 };
 
